fd90e68b2db2c5c14ca97b2ec4daf41e7102c576
dogus <dogusdemirkiran@gmail.com>
2020-06-22 01:38:45 +0300

    Internet Settings

--- app/src/main/java/com/sec/android/MainActivity.java
+++ app/src/main/java/com/sec/android/MainActivity.java
@@ -2,7 +2,12 @@ package com.sec.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        api();
+    }
+
+
+    public boolean networkConnection(){
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isAvailable() && connectivityManager.getActiveNetworkInfo().isConnected()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void api(){
+        if(networkConnection()){
+            //WebView webView = new
+        }else{
+            Toast.makeText(getApplicationContext(),"No Connect",Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 }

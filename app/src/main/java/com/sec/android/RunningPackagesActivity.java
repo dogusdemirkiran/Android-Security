773820208e352b1f71da4bcb3ce5d090ee9209c4
dogus <dogusdemirkiran@gmail.com>
2020-07-01 23:57:58 +0300

    Packages

---
+++ app/src/main/java/com/sec/android/RunningPackagesActivity.java
@@ -0,0 +1,14 @@
+package com.sec.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RunningPackagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_running_packages);
+    }
+}
